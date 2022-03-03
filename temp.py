1b2bb63a42491a109a5e515df944ac6148bdf30b
TORRES CHARRIS JESSICA <jtorres67@cuc.edu.co>
2022-03-02 19:48:30 -0500

    Ejercicio 2

--- temp.py
+++ temp.py
@@ -7,3 +7,14 @@ This is a temporary script file.
 #1
 
 #2
+per1= int(input('Ingrese el dinero invertido por la primera persona: '))
+per2= int(input('Ingrese el dinero invertido por la segunda persona: '))
+per3= int(input('Ingrese el dinero invertido por la segunda persona: '))
+
+total = per1+per2+per3
+p1= (per1/total)*100
+p2= (per2/total)*100
+p3= (per3/total)*100
+print( "La persona 1 invirtió un %", p1)
+print("La persona 2 invirtió un %", p2)
+print( "La persona 3 invirtió un %", p3 ,"El valor total invertido es:", total)
